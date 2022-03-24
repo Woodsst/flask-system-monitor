84b7e7e042fb277e4e9e5e03b9dc3164f31e9628
wood <amper.sil.tok@gmail.com>
2022-03-24 15:45:22 +0300

    rework authorization for postgresql

--- server/authorization.py
+++ server/authorization.py
@@ -1,41 +1,45 @@
 import base64
+import datetime
 import json
 import logging
 
+import psycopg
+
+conn = psycopg.connect(dbname='clients', user='wood', password='123')
+
+cur = conn.cursor()
+
 
 def authorization(username: str, password: str) -> bool or int:
-    with open('clients.csv', 'r', encoding='utf-8') as file:
-        for string in file.readlines():
-            strip = string.strip()
-            _, client_id = strip.split(';')
-            uniq_id = base64.b64encode(f'{username}:{password}'.encode())
-            uniq_id = uniq_id.decode()
-            if client_id == uniq_id:
-                return client_id
+    uniq_id = base64.b64encode(f'{username}:{password}'.encode())
+    uniq_id = uniq_id.decode()
+    cur.execute('SELECT uniq_id FROM clients WHERE uniq_id = %s', (uniq_id,))
+    client_id = cur.fetchone()[0]
+    if client_id is not None and client_id == uniq_id:
+        conn.commit()
+        return client_id
+    conn.commit()
     return False
 
 
 def add_client(username: str, password: str) -> int:
     uniq_id = base64.b64encode(f'{username}:{password}'.encode())
     uniq_id = uniq_id.decode()
-    with open('clients.csv', 'a', encoding='utf-8') as file:
-        file.write(f'{username};{uniq_id}\n')
+    cur.execute('INSERT INTO clients (username, uniq_id, registration_date) VALUES (%s, %s, %s)',
+                (username, uniq_id, datetime.datetime.now()))
     with open(f'{username}_system_load.csv', 'w', encoding='utf-8') as file:
         file.write('time;cpu;memory;storage\n')
+    conn.commit()
     return uniq_id
 
 
 def user_verification(user_name: str) -> bool:
-    try:
-        with open('clients.csv', 'r', encoding='utf-8') as file:
-            for string in file.readlines():
-                strip = string.strip()
-                username, _ = strip.split(';')
-                if user_name == username:
-                    return True
-            return False
-    except FileNotFoundError:
-        return False
+    cur.execute("SELECT username FROM clients WHERE username = %s ", (user_name,))
+    if cur.fetchone() is not None:
+        conn.commit()
+        return True
+    conn.commit()
+    return False
 
 
 def error_authorization(request) -> dict:
@@ -46,12 +50,12 @@ def error_authorization(request) -> dict:
 
 
 def id_verification(client_id: str) -> str or bool:
-    with open('clients.csv', 'r', encoding='utf-8') as file:
-        for string in file.readlines():
-            strip = string.strip()
-            username, client_id_in_file = strip.split(';')
-            if client_id == client_id_in_file:
-                return username
+    cur.execute('SELECT uniq_id, username FROM clients WHERE uniq_id = %s', (client_id,))
+    client_id_in_base, username = cur.fetchone()
+    if client_id is not None and client_id == client_id_in_base:
+        conn.commit()
+        return username
+    conn.commit()
     return False
 
 
